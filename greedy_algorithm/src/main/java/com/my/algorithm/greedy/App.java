df208ce31f16845e7a3dfcb2df9bdb51ffdd5876
naht94 <naht94@hanmail.net>
2024-11-29 17:27:55 +0900

    first commit

---
+++ greedy_algorithm/src/main/java/com/my/algorithm/greedy/App.java
@@ -0,0 +1,13 @@
+package com.my.algorithm.greedy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
